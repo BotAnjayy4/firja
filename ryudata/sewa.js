a260a2ded32ec902be50f098247dadec7305d71f
BotAnjayy4 <99236727+BotAnjayy4@users.noreply.github.com>
2022-03-07 20:49:17 +0700

    Update sewa.js

--- ryudata/sewa.js
+++ ryudata/sewa.js
@@ -8,6 +8,12 @@ exports.sewa = () => {
 ┃ *PERMANEN : Rp 12.000*
 ┗━━━━━━
 ┏━━━━━━
+┃𝗛𝗔𝗥𝗚𝗔 *JadiBot*  :
+┃ *1 MINGGU : Rp 2.000*
+┃ *3 MINGGU : Rp 5.000*
+┃ *PERMANEN : Rp 20.000*
+┗━━━━━━ Note:Wajib Pake 2HP
+┏━━━━━━
 ┃𝗙𝗜𝗧𝗨𝗥 𝗕𝗢𝗧 :
 ┃ *⚔️ Antilink Group*
 ┃ *👨‍🎨 Buat Stiker*
